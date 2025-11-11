b07e4646cabc82217f1e99127618abb9a10ff857
Deep Mhabdi <dmmhabdi2701@gmail.com>
2025-11-11 21:11:04 +0530

    list, tuple, set manipulation

--- basic_objectes.py
+++ basic_objectes.py
@@ -1,7 +1,23 @@
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #list
 
-result = a[1:5] #slicing
+b = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) #tuple
 
-a.append(11) #append
+c = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} #set
 
+convert = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+
+}
+
+convert[7] = 'seven' #adding new key-value pair to dictionary
+
+del convert[4] #deleting key-value pair from dictionary
+print(convert)
+
+del a[2] #deleting item from list
 print(a)
