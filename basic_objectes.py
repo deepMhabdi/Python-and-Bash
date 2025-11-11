acabdd8e0a9cf1883ff1ce14e020336ede43bebf
Deep Mhabdi <dmmhabdi2701@gmail.com>
2025-11-11 13:31:07 +0530

    basic object manipulation and data types examples

---
+++ basic_objectes.py
@@ -0,0 +1,7 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #list
+
+result = a[1:5] #slicing
+
+a.append(11) #append
+
+print(a)
---
+++ my_datatypes.py
@@ -0,0 +1,11 @@
+a = 10
+
+b = "this is a normal string"
+
+c = True 
+
+d = 5.67
+
+result = a + 10
+
+print(result)
