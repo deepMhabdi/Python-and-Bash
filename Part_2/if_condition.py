4325e03d210a6ca292ebea016c8187f2d070ee63
Deep Mhabdi <dmmhabdi2701@gmail.com>
2025-11-14 09:22:22 +0530

    Add grade calculation script based on user input marks

---
+++ Part_2/if_condition.py
@@ -0,0 +1,19 @@
+marks = int(input("HI, ENTER YOUR MARKS: \n"))
+
+if marks >= 90:
+    grade = 'A'
+    
+elif marks >= 80:
+    grade = 'B'
+
+elif marks >= 70:
+    grade = 'C'
+
+elif marks >= 60:
+    grade = 'D' 
+    
+else:
+    grade = 'F'
+    
+print("Your Grade is: ", grade)
+
