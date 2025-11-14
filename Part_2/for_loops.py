0557e4f0ac4406f609453479ba3183369fa4cf42
Deep Mhabdi <dmmhabdi2701@gmail.com>
2025-11-14 09:40:35 +0530

    Add for loop example with conditional printing

---
+++ Part_2/for_loops.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    if i == 1:
+        print("One")
+        
+    elif i ==2:
+        print("Two")
+        
+    elif i ==3:
+        print("Three")
+    else:
+        print("Other")
